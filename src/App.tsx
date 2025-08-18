e2f52ba6598aa0c7cf1b9c9a03126a61b57bc632
trungsqrt <nguyennhumay901@gmail.com>
2025-08-18 22:47:32 +0700

    initial

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+import Header from "./components/Header";
+import AppRoutes from "./routes/AppRoutes";
+import Footer from "./components/Footer";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <AppRoutes />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
