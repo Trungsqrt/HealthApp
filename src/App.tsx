4a5d57ed462809ac98da4d14b92624d9650eeccb
trungsqrt <nguyennhumay901@gmail.com>
2025-08-19 11:13:06 +0700

    footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Header from "./components/Header/Header";
 
 // import AppRoutes from "./routes/AppRoutes";
-// import Footer from "./components/Footer";
+// import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
